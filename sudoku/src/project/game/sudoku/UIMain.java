80c52c3c99b4b925a1cd1536d429ba53d6b72f29
joaojoga <joaovictormontanaro@gmail.com>
2025-04-03 00:26:26 -0300

    Colocando os metodos dos novos services

---
+++ sudoku/src/project/game/sudoku/UIMain.java
@@ -0,0 +1,16 @@
+package project.game.sudoku;
+
+import project.game.sudoku.ui.custom.screen.MainScreen;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+public class UIMain {
+    public static void main(String[] args) {
+        var gameConfig = Stream.of(args).
+                collect(toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
+        var mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();
+    }
+}
